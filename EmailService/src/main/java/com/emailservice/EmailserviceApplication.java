7d9c7039337699b614945bbb70e1b670b93df184
varun.belekar@somaiya.edu <varun.belekar@somaiya.edu>
2021-11-29 22:02:52 +0530

    Removing whitespaces.

--- EmailService/src/main/java/com/emailservice/EmailserviceApplication.java
+++ EmailService/src/main/java/com/emailservice/EmailserviceApplication.java
@@ -6,10 +6,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class EmailserviceApplication {
-
+public class EmailServiceApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(EmailserviceApplication.class, args);
+		SpringApplication.run(EmailServiceApplication.class, args);
 	}
-
 }
